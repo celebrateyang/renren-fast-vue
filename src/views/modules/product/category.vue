519fca9ea19d685f149d0a403e877bf47fcb78b0
zhuqyang <zhuqyang@ebay.com>
2021-01-24 16:17:25 +0700

    category 添加, 删除,checkbox,node-key

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,9 +1,18 @@
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    @node-click="handleNodeClick"
-  ></el-tree>
+  <el-tree :data="menus" :props="defaultProps" 
+  :expand-on-click-node="false" show-checkbox node-key="catId">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button v-if="node.level<=2" type="text" size="mini" @click="() => append(data)">
+          Append
+        </el-button>
+        <el-button v-if="node.childNodes.length==0" type="text" size="mini" @click="() => remove(node, data)">
+          Delete
+        </el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
@@ -18,22 +27,27 @@ export default {
     };
   },
   methods: {
-    handleNodeClick(data) {
-      console.log(data);
+    
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        console.log("成功获取到数据=>", data.data);
+        this.menus = data.data;
+      });
     },
-    getMenus(){
-        this.$http({
-          url: this.$http.adornUrl('/product/category/list/tree'),
-          method: 'get',
-        }).then(({data})=>{
-          console.log("成功获取到数据=>",data.data)
-          this.menus=data.data;
-          })
-    }
+    append(data) {
+        console.log(data)
+      },
+
+      remove(node, data) {
+        console.log(node,data)
+      },
   },
-  created(){
-      this.getMenus()
-  }
+  created() {
+    this.getMenus();
+  },
 };
 </script>
 
