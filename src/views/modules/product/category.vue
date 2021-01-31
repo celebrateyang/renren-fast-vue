a1d3f8ba29adb5426b1846274ace90a4cd4407f2
zhuqyang <zhuqyang@ebay.com>
2021-01-31 14:16:36 +0700

    1. 实现编辑category,并和append category公用弹出对话框

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -19,6 +19,10 @@
           >
             Append
           </el-button>
+
+          <el-button type="text" size="mini" @click="() => edit(data)">
+            Edit
+          </el-button>
           <el-button
             v-if="node.childNodes.length == 0"
             type="text"
@@ -31,15 +35,24 @@
       </span>
     </el-tree>
 
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+    <el-dialog :title="title" :visible.sync="dialogVisible" width="30%" :close-on-click-modal="false">
       <el-form :model="category">
         <el-form-item label="分类名称">
           <el-input v-model="category.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input
+            v-model="category.productUnit"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCategory">确 定</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -49,7 +62,18 @@
 export default {
   data() {
     return {
-      category: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 },
+      title: "",
+      dialogType: "", //edit,add
+      category: {
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        catId: null,
+        icon: "",
+        productUnit: "",
+      },
       dialogVisible: false,
       menus: [],
       defaulstexKey: [],
@@ -72,28 +96,75 @@ export default {
     append(data) {
       console.log(data);
       this.dialogVisible = true;
-       this.category.parentCid = data.catId;
-       this.category.catLevel = data.catLevel*1+1;
+      this.title = "添加分类";
+      this.dialogType = "add";
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+    },
+    edit(data) {
+      console.log("要修改的数据=>", data);
+      this.dialogVisible = true;
+      this.title = "修改分类";
+      this.dialogType = "edit";
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get",
+      }).then(({ data }) => {
+        console.log("要回显的数据=>", data);
+        this.category.name = data.data.name;
+        this.category.catId = data.data.catId;
+        this.category.icon = data.data.icon;
+        this.category.productUnit = data.data.productUnit;
+        this.category.parentCid = data.data.parentCid;
+      });
+    },
+    submitData(data) {
+      if (this.dialogType == "edit") {
+        this.editCategory();
+      }
+      if (this.dialogType == "add") {
+        this.addCategory();
+      }
     },
     // 添加分类
     addCategory() {
       console.log("提交的分类数据=>", this.category);
       this.$http({
-      url: this.$http.adornUrl('/product/category/save'),
-      method: 'post',
-      data: this.$http.adornData(this.category, false)
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
       }).then(({ data }) => {
         this.$message({
-              message: "保存成功!",
-              type: "success",
-            });
+          message: "保存成功!",
+          type: "success",
+        });
         //关闭对话框
-        this.dialogVisible=false;
+        this.dialogVisible = false;
         //刷新菜单
         this.getMenus();
         //设置默认展开的菜单
         this.defaulstexKey = [this.category.parentCid];
-       });
+      });
+    },
+    editCategory() {
+      var {name,catId,icon,productUnit} = this.category;//解构
+      //var data = {name,catId,icon,productUnit}; key:value一致,可以省略 key. 这一步还可以省略,直接写在adornData里面
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData({name,catId,icon,productUnit}, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "修改成功!",
+          type: "success",
+        });
+        //关闭对话框
+        this.dialogVisible = false;
+        //刷新菜单
+        this.getMenus();
+        //设置默认展开的菜单
+        this.defaulstexKey = [this.category.parentCid];
+      });
     },
     remove(node, data) {
       var ids = [data.catId];
